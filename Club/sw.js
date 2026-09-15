4641f175ce14047943bea6a907fe89f3139e025d
ROSTICCERIANAPOLETANA <rosticceriaitalianatenerife@gmail.com>
2026-09-15 21:54:55 +0100

    Forza aggiornamento PWA su iPhone e rinnova cache

--- Club/sw.js
+++ Club/sw.js
@@ -1,5 +1,5 @@
-const CACHE='el-capricho-club-v2';
-const ASSETS=['./','./index.html','./manifest.webmanifest','../logo.png'];
+const CACHE='el-capricho-club-v3';
+const ASSETS=['./manifest.webmanifest','../logo.png'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
@@ -7,20 +7,31 @@ self.addEventListener('install',event=>{
 });
 
 self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-  self.clients.claim();
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch',event=>{
   const req=event.request;
   if(req.method!=='GET') return;
+
   if(req.mode==='navigate'){
-    event.respondWith(fetch(req).then(res=>{
-      const copy=res.clone();
-      caches.open(CACHE).then(cache=>cache.put('./index.html',copy));
-      return res;
-    }).catch(()=>caches.match('./index.html')));
+    event.respondWith(
+      fetch(req,{cache:'no-store'})
+        .catch(()=>caches.match('./index.html'))
+    );
     return;
   }
-  event.respondWith(caches.match(req).then(cached=>cached||fetch(req)));
+
+  event.respondWith(
+    fetch(req,{cache:'no-store'})
+      .then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE).then(cache=>cache.put(req,copy));
+        return res;
+      })
+      .catch(()=>caches.match(req))
+  );
 });
