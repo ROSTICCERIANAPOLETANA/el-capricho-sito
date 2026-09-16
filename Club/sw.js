220de11d5ca4e99a5bf15a25f6117ba6192645af
ROSTICCERIANAPOLETANA <rosticceriaitalianatenerife@gmail.com>
2026-09-16 13:23:29 +0100

    Aggiorna cache PWA dopo miglioramenti installazione

--- Club/sw.js
+++ Club/sw.js
@@ -1,37 +1,5 @@
-const CACHE='el-capricho-club-v3';
-const ASSETS=['./manifest.webmanifest','../logo.png'];
-
-self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch',event=>{
-  const req=event.request;
-  if(req.method!=='GET') return;
-
-  if(req.mode==='navigate'){
-    event.respondWith(
-      fetch(req,{cache:'no-store'})
-        .catch(()=>caches.match('./index.html'))
-    );
-    return;
-  }
-
-  event.respondWith(
-    fetch(req,{cache:'no-store'})
-      .then(res=>{
-        const copy=res.clone();
-        caches.open(CACHE).then(cache=>cache.put(req,copy));
-        return res;
-      })
-      .catch(()=>caches.match(req))
-  );
-});
+const CACHE='el-capricho-club-v4';
+const ASSETS=['./','./manifest.webmanifest','../logo.png'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{const req=event.request;if(req.method!=='GET')return;if(req.mode==='navigate'){event.respondWith(fetch(req,{cache:'no-store'}).then(res=>{const copy=res.clone();caches.open(CACHE).then(cache=>cache.put('./',copy));return res}).catch(()=>caches.match('./')));return}event.respondWith(fetch(req,{cache:'no-store'}).then(res=>{const copy=res.clone();caches.open(CACHE).then(cache=>cache.put(req,copy));return res}).catch(()=>caches.match(req)))})
