45556f9608736b8bbbfb7c05e6a0641e44dee3b8
ROSTICCERIANAPOLETANA <rosticceriaitalianatenerife@gmail.com>
2026-09-16 15:20:48 +0100

    Bump Club cache after recovery and biometric update

--- Club/sw.js
+++ Club/sw.js
@@ -1,4 +1,4 @@
-const CACHE='el-capricho-club-v5';
+const CACHE='el-capricho-club-v6';
 const SHELL=['./','./index.html','./manifest.webmanifest','../logo.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('el-capricho-club-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
