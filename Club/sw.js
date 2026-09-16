e27b20e51e71cfa6c97b03f9819320dc36d098b8
ROSTICCERIANAPOLETANA <rosticceriaitalianatenerife@gmail.com>
2026-09-16 15:25:30 +0100

    Club: refresh PWA cache v7

--- Club/sw.js
+++ Club/sw.js
@@ -1,4 +1,4 @@
-const CACHE='el-capricho-club-v6';
+const CACHE='el-capricho-club-v7';
 const SHELL=['./','./index.html','./manifest.webmanifest','../logo.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('el-capricho-club-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
